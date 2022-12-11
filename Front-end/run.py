f94bfd0a6db14dddda07944f36394325d6becbbb
Essawey <m.abdelmaged@nu.edu.eg>
2022-12-11 08:02:39 +0200

    Update run.py

--- Front-end/run.py
+++ Front-end/run.py
@@ -1,8 +1,20 @@
-create_table_user = "CREATE TABLE user (UserID INT(10) NOT NULL AUTO_INCREMENT,FristName VARCHAR(20) NOT NULL,LastName VARCHAR(20) DEFAULT NULL,Username VARCHAR(20) NOT NULL,Email VARCHAR(30) NOT NULL,PhoneNumber VARCHAR(11) NOT NULL,Password VARCHAR(20) NOT NULL,Gender VARCHAR(6) NOT NULL,PRIMARY KEY (USERID));"
+create_table_user = """
+CREATE TABLE IF NOT EXISTS user (
+	UserID INT NOT NULL AUTO_INCREMENT UNIQUE,
+    FristName VARCHAR(20) NOT NULL,
+    LastName VARCHAR(20) DEFAULT NULL,
+    Username VARCHAR(20) NOT NULL UNIQUE,
+    Email VARCHAR(30) NOT NULL UNIQUE,
+    PhoneNumber VARCHAR(11) NOT NULL UNIQUE,
+    Password VARCHAR(20) NOT NULL,
+    Gender VARCHAR(6) NOT NULL,
+    PRIMARY KEY (USERID));
+"""
 
-from flask import Flask, render_template , request , redirect
+from flask import Flask, render_template , request , redirect , flash
 import pymysql
 import database_detail
+from waitress import serve
 
 conn = pymysql.connect(
     host = database_detail.host,
@@ -12,16 +24,11 @@ conn = pymysql.connect(
     )
 
 
-
-
 app = Flask(__name__, static_folder='static')
+app.secret_key = 'super secret key LOL'
 
-
-try:
-    cursor = conn.cursor()
-    cursor.execute(create_table_user)
-except pymysql.err.OperationalError:
-    pass
+cursor = conn.cursor()
+cursor.execute(create_table_user)
 
 
 
@@ -33,7 +40,7 @@ def home():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method=="POST":
-        print(request.form)
+        # print(request.form)
 
         cursor.execute("SELECT COUNT(Username) FROM user WHERE username = %s AND password = %s LIMIT 0, 1;",
             (
@@ -45,27 +52,49 @@ def login():
         result=cursor.fetchone()
         if(result == (1,)):
             return redirect('/home')
+        else:
+            flash("Incorrect Email or Password")
     return render_template('login.html')
+
 
 @app.route("/registration", methods=["GET", "POST"])
 def registration():
     if request.method=="POST":
-        # print(request.form)
-        cursor.execute("INSERT INTO user (FristName,LastName,Username,Email,PhoneNumber,Password,Gender) VALUES (%s,%s,%s,%s, %s, %s, %s);",
-            (
-                request.form.get("name").split(" ")[0],
-                request.form.get("name").split(" ")[1],
-                request.form.get("username"),
-                request.form.get("email"),
-                request.form.get("number"),
-                request.form.get("password"),
-                request.form.get("Gender")
-            )
-        )   
-        conn.commit()
-        return redirect('/home')
+            
+        try:
+            cursor.execute("INSERT INTO user (FristName,LastName,Username,Email,PhoneNumber,Password,Gender) VALUES (%s,%s,%s,%s, %s, %s, %s);",
+                (
+                    request.form.get("name").split(" ")[0],
+                    request.form.get("name").split(" ")[1],
+                    request.form.get("username"),
+                    request.form.get("email"),
+                    request.form.get("number"),
+                    request.form.get("password"),
+                    request.form.get("Gender")
+                )
+            )   
+            conn.commit()
+            return redirect('/home')
+        except pymysql.err.IntegrityError:
+            flash("Username or Email already Exists")
     return render_template('registration.html')
 
 
+@app.route("/favorite")
+def favorite():
+    return render_template('favorite.html')
+
+@app.route("/history")
+def history():
+    return render_template('history.html')
+
+@app.route("/recommended")
+def recommended():
+    return render_template('recommended.html')
+
+@app.route("/watch_list")
+def watch_list():
+    return render_template('watch_list.html')
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    serve(app, host="127.0.0.1", port=5000)
