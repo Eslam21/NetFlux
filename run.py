6dd1fbea58a288a82fdf37473b59e1bcf3cfcaba
Mariam Barakat <Ma.Barakat@nu.edu.eg>
2022-12-14 11:40:57 +0200

    edited html

--- run.py
+++ run.py
@@ -58,7 +58,7 @@ def watch_list():
 
 @app.route("/profile-page",  methods=["GET", "POST"])
 def open_profile():
-    return render_template('profile-page')
+    return render_template('profile-page.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
