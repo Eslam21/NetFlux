c9c5d360302f5bc699a087ad89aa954786d734b7
Mariam Barakat <Ma.Barakat@nu.edu.eg>
2022-12-29 16:01:38 +0200

    some changes

--- Testing/JS/run2.py
+++ Testing/JS/run2.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     user = {'firstname': 'Harry', 'lastname': 'Potter', "Age":[18,27,33,88,5]}
-    return render_template("Java_script.html", user=user)
+    x = 10
+    return render_template("Java_script.html", user= user , x = type(x))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
