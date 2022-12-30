87416ce9483623ee3f99f3e5387a51a6358a4a00
Eslam21 <Es.Ahmed@nu.edu.eg>
2022-12-30 15:23:49 +0200

    Revert "Merge branch 'main' of https://github.com/Eslam21/NetFlux"
    This reverts commit 25f6f24845b8b73b725372e376261fe55f143a19, reversing
    changes made to e3b8814561b2fb413f0b49da824118223c81bf8c.

--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-# for quick testing 
-
-import mysql.connector
-
-connection = mysql.connector.connect(
-    user="SWE", password="123456789000", host="localhost", database="netflux"
-)
-
-cursor = connection.cursor()
